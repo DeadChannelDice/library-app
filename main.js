38130af01ef4e60c1ad4e1bd653dcb07325a0556
DeadChannelDice <michael.charpin@gmail.com>
2023-07-05 17:45:02 -0400

    Basic modal opeing and closing.

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+const currentRead = document.querySelector('#current-read-area')
+const stackArea = document.querySelector('#stack-area')
+const shelfArea = document.querySelector('#shelf-area')
+const addBtn = document.querySelector('#add-book-btn')
+const addBookModal = document.querySelector('#add-book-modal')
+const moveBookCurrentBtn = document.querySelector('#current-btn')
+const moveBookStackBtn = document.querySelector('#stack-btn')
+const moveBookShelfBtn = document.querySelector('#shelf-btn')
+const deleteBookBtn = document.querySelector('#delete-btn')
+const exitBtn = document.querySelector('#modal-exit-btn')
 
+addBtn.addEventListener('click', () => {
+  addBookModal.showModal()
+})
+
+exitBtn.addEventListener('click', () => {
+  addBookModal.close()
+})
